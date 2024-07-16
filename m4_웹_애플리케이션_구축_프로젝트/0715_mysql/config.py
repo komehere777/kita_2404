99d37fb8f31ff38897189905bdcbbb1347ea539b
Rodney Kim <komehere@knou.ac.kr>
2024-07-16 09:44:45 +0900

    Add files via upload

---
+++ m4_웹_애플리케이션_구축_프로젝트/0715_mysql/config.py
@@ -0,0 +1,4 @@
+SECRET_KEY = 'c1f66bc6e9951b1c4fd6df15798fda5e09684f707bdb2e56'
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///tasks.db'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://kita:kita@localhost:3306/kita_db'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
